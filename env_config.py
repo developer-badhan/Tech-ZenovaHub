d5ca966c0d867fa57650b779c6fb77641ff78b54
Badhan Sarkar <badhansarkar164@gmail.com>
2025-08-01 22:03:47 +0530

    maintaining sercret

---
+++ env_config.py
@@ -0,0 +1,18 @@
+import environ
+
+env = environ.Env(
+    DEBUG=(bool, False),
+
+    ALLOWED_HOSTS=(list, []),
+
+    SECRET_KEY=(str, ''),
+
+    PGSQL_NAME=(str, ''),
+    PGSQL_USER=(str, ''),
+    PGSQL_PASSWORD=(str, ''),
+    PGSQL_HOST=(str, ''),
+    PGSQL_PORT=(str, ''),
+)
+
+env.read_env()
+
