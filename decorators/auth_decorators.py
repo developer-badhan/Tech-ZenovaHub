90ec894b1ed24bd1f05b475edda642661614186e
Badhan Sarkar <badhansarkar164@gmail.com>
2025-08-15 22:13:58 +0530

    admin decorators

--- decorators/auth_decorators.py
+++ decorators/auth_decorators.py
@@ -36,4 +36,13 @@ def staff_required(view_func):
         return view_func(view_self, request, *args, **kwargs)
     return wrapper
 
+def admin_required(view_func):
+    @wraps(view_func)
+    def wrapper(view_self, request, *args, **kwargs):
+        if not request.session.get('is_authenticated') or request.session.get('user_role') != Role.ADMIN:
+            current_view_name = resolve(request.path_info).url_name
+            if current_view_name != 'user_login':
+                return redirect('user_login')
+        return view_func(view_self, request, *args, **kwargs)
+    return wrapper
 
