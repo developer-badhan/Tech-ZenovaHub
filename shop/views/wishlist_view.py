12c3b63c2d6e329e671f8a5cbc8afe3f528a685b
Badhan Sarkar <badhansarkar164@gmail.com>
2025-08-28 10:11:47 +0530

    complete work on wishlist

--- shop/views/wishlist_view.py
+++ shop/views/wishlist_view.py
@@ -1,17 +1,51 @@
 from django.shortcuts import render, redirect
 from django.views import View
+from decorators import signin_required, customer_required, inject_authenticated_user
+from shop.services import wishlist_service 
+
 
 
 # Wishlist List View
 class WishlistListView(View):
-    pass
+    @signin_required
+    @customer_required
+    @inject_authenticated_user
+    def get(self, request):
+        wishlist_items = wishlist_service.get_user_wishlist(request.user)
+        return render(request, "wishlist/wishlist.html", {"wishlist": wishlist_items})
 
 
 # Wishlist Add View
 class WishlistAddView(View):
-    pass
+    @signin_required
+    @customer_required
+    @inject_authenticated_user
+    def get(self, request, product_id):
+        return render(request, "wishlist/wishlist_add.html", {"product_id": product_id})
+    
+    @signin_required
+    @customer_required
+    @inject_authenticated_user
+    def post(self, request, product_id):
+        item = wishlist_service.add_to_wishlist(request.user, product_id)
+        if item:
+            return redirect("wishlist")
+        return redirect("wishlist")
 
+        
 # Wishlist Remove View
 class WishlistRemoveView(View):
-    pass
+    @signin_required
+    @customer_required
+    @inject_authenticated_user
+    def get(self, request, product_id):
+        return render(request, "wishlist/wishlist_remove.html", {"product_id": product_id})
+
+    @signin_required
+    @customer_required
+    @inject_authenticated_user
+    def post(self, request, product_id):
+        success = wishlist_service.remove_from_wishlist(request.user, product_id)
+        return redirect("wishlist")
+
 
