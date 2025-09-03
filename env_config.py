48fde001ffa6568ab10c435b667b0713c054222c
Badhan Sarkar <badhansarkar164@gmail.com>
2025-09-03 23:27:04 +0530

    implement otp verification

--- env_config.py
+++ env_config.py
@@ -4,21 +4,29 @@ import environ
 
 # Initialize environment variable handler with default values and types
 env = environ.Env(
-    DEBUG=(bool, False),                 # Default DEBUG to False
-    ALLOWED_HOSTS=(list, []),           # Default ALLOWED_HOSTS to empty list
-    SECRET_KEY=(str, ''),               # Django secret key for cryptographic signing
+    DEBUG=(bool, False),                
+    ALLOWED_HOSTS=(list, []),          
+    SECRET_KEY=(str, ''),               
 
     # PostgreSQL database configuration
-    PGSQL_NAME=(str, ''),               # Database name
-    PGSQL_USER=(str, ''),               # Database user
-    PGSQL_PASSWORD=(str, ''),           # Database password
-    PGSQL_HOST=(str, ''),               # Database host
-    PGSQL_PORT=(str, ''),               # Database port
+    PGSQL_NAME=(str, ''),              
+    PGSQL_USER=(str, ''),              
+    PGSQL_PASSWORD=(str, ''),           
+    PGSQL_HOST=(str, ''),              
+    PGSQL_PORT=(str, ''),              
 
-    SESSION_COOKIE_NAME=(str, 'zenovahub_sessionid'), # Session cookie name
-    SESSION_COOKIE_SECURE=(bool, True), # Whether to use HTTPS for the session cookie
-    SESSION_COOKIE_HTTPONLY=(bool, True), # Whether to make the session cookie HTTP-only
-    SESSION_COOKIE_SAMESITE=(str, 'Lax') # SameSite attribute for the session cookie
+    # Session cookie settings
+    SESSION_COOKIE_NAME=(str, 'zenovahub_sessionid'), 
+    SESSION_COOKIE_SECURE=(bool, True),
+    SESSION_COOKIE_HTTPONLY=(bool, True), 
+    SESSION_COOKIE_SAMESITE=(str, 'Lax') 
+
+    # Email settings
+    EMAIL_HOST=(str, '')
+    EMAIL_PORT=(int, 587)
+    EMAIL_USE_TLS=(bool, True)
+    EMAIL_HOST_USER=(str, '')
+    EMAIL_HOST_PASSWORD=(str, '')
 )
 
 # Read variables from a .env file located at the project root
