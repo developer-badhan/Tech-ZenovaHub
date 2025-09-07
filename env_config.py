56aadc16082f56a1aa51b2aadeb8d0ba05808a40
Badhan Sarkar <badhansarkar164@gmail.com>
2025-09-07 11:31:08 +0530

    intoalized

--- env_config.py
+++ env_config.py
@@ -19,14 +19,14 @@ env = environ.Env(
     SESSION_COOKIE_NAME=(str, 'zenovahub_sessionid'), 
     SESSION_COOKIE_SECURE=(bool, True),
     SESSION_COOKIE_HTTPONLY=(bool, True), 
-    SESSION_COOKIE_SAMESITE=(str, 'Lax') 
+    SESSION_COOKIE_SAMESITE=(str, 'Lax'),
 
     # Email settings
-    EMAIL_HOST=(str, '')
-    EMAIL_PORT=(int, 587)
-    EMAIL_USE_TLS=(bool, True)
-    EMAIL_HOST_USER=(str, '')
-    EMAIL_HOST_PASSWORD=(str, '')
+    # EMAIL_HOST=(str, '')
+    # EMAIL_PORT=(int, 587)
+    # EMAIL_USE_TLS=(bool, True)
+    # EMAIL_HOST_USER=(str, '')
+    # EMAIL_HOST_PASSWORD=(str, '')
 )
 
 # Read variables from a .env file located at the project root
